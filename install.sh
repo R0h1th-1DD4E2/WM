4b81c8baf4b089481992a2c34510381659442a4d
R0h1th-1DD4E2 <113779465+R0h1th-1DD4E2@users.noreply.github.com>
2023-10-21 19:44:28 +0530

    Added Ubuntu install Script

--- install.sh
+++ install.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+echo "!!!!!!!!------ Warning ------!!!!!!!!!"
+echo "You will be Prompt to enter sudo password, Use it at your own risk as it involves installing packages and it hasn't been tested properly"
+echo "--------------------------------------"
+
+# Prompt the user for confirmation
+read -p "Do you want to run my script? (y/n): " choice
+
+# Check the user's choice
+if [ "$choice" = "y" ] || [ "$choice" = "Y" ]; then
+    echo "Running the install script..."
+else
+    echo "Installation canceled."
+    exit 1
+fi
+
 #step 1 - install the i3 window manager
 distro=$(cat /etc/os-release | awk -F= '/NAME=/ {gsub(/"/, "", $2); print $2; exit}')
 
@@ -11,7 +26,48 @@ case $distro in
         #i will fill with the dependencies to install i3, polybar, alacrity, rofi , feh, neofetch
         ;;
     "Ubuntu")
-        #i will fill with the dependencies to install i3, polybar, alacrity, rofi , feh, neofetch
+        #Installing i3 rofi feh
+
+        echo "Installing i3 rofi feh .........."
+        sudo apt -y install i3 rofi feh 
+
+        #All the git repos and source build is done in this folder
+        mkdir build_files
+
+        #installing alacritty dependencies
+        sudo apt -y install cmake pkg-config libfreetype6-dev libfontconfig1-dev libxcb-xfixes0-dev libxkbcommon-dev python3
+
+        #Installing Rust
+        sudo curl https://sh.rustup.rs -sSf | sh
+
+        cd build_files
+
+        #cloning alacritty repo
+        git clone https://github.com/alacritty/alacritty.git
+        cd alacritty
+
+        cargo build --release
+
+        echo "Verifying ............ "
+
+        if infocmp alacritty &> /dev/null; then
+            echo "Alacritty is installed"
+        else
+            echo "Alacritty is not installed correctly."
+            sudo tic -xe alacritty,alacritty-direct extra/alacritty.info
+        fi
+
+        #adding binaries
+        sudo cp target/release/alacritty /usr/local/bin
+
+        #autocompletion
+        echo "source $(pwd)/extra/completions/alacritty.bash" >> ~/.bashrc
+
+        cd ..
+
+        #Installing PICOM
+        
+
         ;;
     *)
         echo "Distribution not recognized. Please install dependencies manually."
