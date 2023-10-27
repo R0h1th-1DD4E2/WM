761003e9523b91910ea30bbecde23fded466f13a
R0h1th-1DD4E2 <113779465+R0h1th-1DD4E2@users.noreply.github.com>
2023-10-27 16:18:10 +0530

    Completed with ubuntu install script

--- install.sh
+++ install.sh
@@ -9,9 +9,11 @@ echo "..##...##.....##"
 echo ".####...#######."
 
 
-echo "!!!!!!!!------ Warning ------!!!!!!!!!"
-echo "You will be Prompt to enter sudo password, Use it at your own risk as it involves installing packages and it hasn't been tested properly"
-echo "--------------------------------------"
+echo "!!!!!!!!---------------- Warning -------------!!!!!!!!!"
+echo "////// You will be Prompt to enter sudo password,//////" 
+echo "// Use it at your own risk as it involves installing //"
+echo "///// packages and it hasn't been tested properly /////" 
+echo "-------------------------------------------------------"
 
 # Prompt the user for confirmation
 read -p "Do you want to run my script? (y/n): " choice
@@ -75,16 +77,63 @@ case $distro in
         cd ..
 
         #Installing PICOM
-        # I'll fill this later 
+        
+        echo "Installing Picom........"
+
+        # Dependencies
+        sudo apt install libconfig-dev libdbus-1-dev libegl-dev libev-dev libgl-dev libpcre2-dev libpixman-1-dev libx11-xcb-dev libxcb1-dev libxcb-composite0-dev libxcb-damage0-dev libxcb-dpms0-dev libxcb-glx0-dev libxcb-image0-dev libxcb-present-dev libxcb-randr0-dev libxcb-render0-dev libxcb-render-util0-dev libxcb-shape0-dev libxcb-util-dev libxcb-xfixes0-dev libxext-dev meson ninja-build uthash-dev
+
+        git clone https://github.com/yshui/picom.git
+
+        cd picom
+
+        # building picom
+        meson setup --buildtype=release build
+        ninja -C build
+
+        #installing picom
+        ninja -C build install
+
+        cd ..
 
         #Installing Oh-my-posh
         curl -s https://ohmyposh.dev/install.sh | bash -s -- -d ./
 
+        if_dir_exists "$HOME/.oh-my-posh"
+        if_dir_exists "$HOME/.local/share/oh-my-posh/themes"
         
+        cp oh-my-posh $HOME/.oh-my-posh
+
+        source_dir="$HOME/.cache/oh-my-posh/themes"
+        destination_dir="$HOME/.local/share/oh-my-posh/themes"
+
+        if [ -d "$source_dir" ]; then
+            mv "$source_dir" "$destination_dir"
+            echo "Themes moved from $source_dir to $destination_dir"
+        else
+            echo "Source directory $source_dir not found."
+        fi
+
+        ohmyposh_path="$HOME/.oh-my-posh"
+        ohmyposh_theme_command="eval \"\$(oh-my-posh init bash --config $HOME/.local/share/oh-my-posh/themes/catppuccin_macchiato.omp.json)\""
+
+        # Adding the paths and configurations to .bashrc
+        if ! grep -q "export PATH=\"$ohmyposh_path\"" "$HOME/.bashrc"; then
+            echo "export PATH=\"$ohmyposh_path\"" >> "$HOME/.bashrc"
+        fi
+
+        if ! grep -q "$ohmyposh_theme_command" "$HOME/.bashrc"; then
+            echo "$ohmyposh_theme_command" >> "$HOME/.bashrc"
+        fi
+
+        # Source the updated .bashrc
+        source "$HOME/.bashrc"
+
+        cd ..
 
         ;;
     *)
-        echo "Distribution not recognized. Please install dependencies manually."
+        echo "Distribution not recognized. This install script is only made for Arch , Fedora and Ubuntu."
         ::
 esac
 
@@ -95,8 +144,8 @@ if_dir_exists() {
     local dir_path="$1"
 
     if [ -d "$dir_path" ]; then
-        return 0  # Directory exists
+        :  # Directory exists
     else
-        return 1  # Directory does not exist
+        mkdir "$dir_path"  # Directory does not exist
     fi
 } 
