76161d6136c6a3ab24b1113d9acc4a2631182f87
R0h1th-1DD4E2 <rohith.010404@gmail.com>
2024-01-12 23:32:03 +0530

    Begin with Arch Script

--- install.sh
+++ install.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+NOT="[\e[1;36mNOTE\e[0m]"
+OK="[\e[1;32mOK\e[0m]"
+ERR="[\e[1;31mERROR\e[0m]"
+ATT="[\e[1;37mATTENTION\e[0m]"
+WARN="[\e[1;35mWARNING\e[0m]"
+ACT="[\e[1;33mACTION\e[0m]"
+LOGIT="install.log"
+
 echo -e "\e[32m ####   ####### \e[0m"
 echo -e "\e[32m ####  ##     ##\e[0m"
 echo -e "\e[32m              ##\e[0m"
@@ -32,172 +40,19 @@ distro=$(cat /etc/os-release | awk -F= '/NAME=/ {gsub(/"/, "", $2); print $2; ex
 
 case $distro in
     "Arch Linux")
-        #i will fill with the dependencies to install i3, polybar, alacrity, rofi , feh, neofetch
-        echo "Installing i3, rofi, feh, neofetch.........."
-        sudo pacman -s neofetch i3-wm rofi rofi-emoji feh alacritty picom 
-
-        read -p "Do you want to install oh-my-posh? (y/n): " opinion
-        if [ "$opinion" = "y" ] || [ "$opinion" = "Y" ]; then
-            echo "Installing Oh-my-posh......."
-            #Installing Oh-my-posh
-        
-            curl -s https://ohmyposh.dev/install.sh | bash -s -- -d ./
-
-            if_dir_exists "$HOME/.oh-my-posh"
-            if_dir_exists "$HOME/.local/share/oh-my-posh/themes"
-            
-            cp oh-my-posh $HOME/.oh-my-posh
-
-            source_dir="$HOME/.cache/oh-my-posh/themes"
-            destination_dir="$HOME/.local/share/oh-my-posh/themes"
-
-            if [ -d "$source_dir" ]; then
-                mv "$source_dir" "$destination_dir"
-                echo "Themes moved from $source_dir to $destination_dir"
-            else
-                echo "Source directory $source_dir not found."
-                echo "Try installing oh-my-posh again manually "
-            fi
-
-            ohmyposh_path="$HOME/.oh-my-posh"
-            ohmyposh_theme_command="eval \"\$(oh-my-posh init bash --config $HOME/.local/share/oh-my-posh/themes/catppuccin_macchiato.omp.json)\""
-
-            # Adding the paths and configurations to .bashrc
-            if ! grep -q "export PATH=\"$ohmyposh_path\"" "$HOME/.bashrc"; then
-                echo "export PATH=\"$ohmyposh_path\"" >> "$HOME/.bashrc"
-            fi
-
-            if ! grep -q "$ohmyposh_theme_command" "$HOME/.bashrc"; then
-                echo "$ohmyposh_theme_command" >> "$HOME/.bashrc"
-            fi
-
-            # Source the updated .bashrc
-            source "$HOME/.bashrc"
-        else
-            echo "Oh-my=posh was not installed ..."
-        fi
-        
-        echo "Moving config files......"
+        # Arch Script here
+        ./scripts/arch.sh
 
         ;;
     "Fedora Linux")
-        #i will fill with the dependencies to install i3, polybar, alacrity, rofi , feh, neofetch
+        # If contributed add it here please
         ;;
     "Ubuntu" | "Debian"*)
-        #Installing i3 rofi feh
-
-        echo "Installing i3, rofi, feh, neofetch.........."
-        sudo apt -y install i3 rofi feh 
-
-        #All the git repos and source build is done in this folder
-        mkdir build_files
-
-        #installing alacritty dependencies
-        sudo apt -y install cmake pkg-config libfreetype6-dev libfontconfig1-dev libxcb-xfixes0-dev libxkbcommon-dev python3 curl
-
-        #Installing Rust
-        sudo curl https://sh.rustup.rs -sSf | sh
-
-        cd build_files
-
-        #cloning alacritty repo
-        git clone https://github.com/alacritty/alacritty.git
-        cd alacritty
-
-        cargo build --release
-
-        echo "Verifying ............ "
-
-        if infocmp alacritty &> /dev/null; then
-            echo "Alacritty is installed"
-        else
-            echo "Alacritty is not installed correctly."
-            sudo tic -xe alacritty,alacritty-direct extra/alacritty.info
-        fi
-
-        #adding binaries
-        sudo cp target/release/alacritty /usr/local/bin
-
-        #autocompletion
-        echo "source $(pwd)/extra/completions/alacritty.bash" >> ~/.bashrc
-
-        cd ..
-
-        #Installing PICOM
-        
-        echo "Installing Picom........"
-
-        # Dependencies
-        sudo apt install libconfig-dev libdbus-1-dev libegl-dev libev-dev libgl-dev libpcre2-dev libpixman-1-dev libx11-xcb-dev libxcb1-dev libxcb-composite0-dev libxcb-damage0-dev libxcb-dpms0-dev libxcb-glx0-dev libxcb-image0-dev libxcb-present-dev libxcb-randr0-dev libxcb-render0-dev libxcb-render-util0-dev libxcb-shape0-dev libxcb-util-dev libxcb-xfixes0-dev libxext-dev meson ninja-build uthash-dev
-
-        git clone https://github.com/yshui/picom.git
-
-        cd picom
-
-        # building picom
-        meson setup --buildtype=release build
-        ninja -C build
-
-        #installing picom
-        ninja -C build install
-
-        cd ..
-
-        #Installing Oh-my-posh
-        echo "Installing Oh-my-posh......."
-
-        curl -s https://ohmyposh.dev/install.sh | bash -s -- -d ./
-
-        if_dir_exists "$HOME/.oh-my-posh"
-        if_dir_exists "$HOME/.local/share/oh-my-posh/themes"
-        
-        cp oh-my-posh $HOME/.oh-my-posh
-
-        source_dir="$HOME/.cache/oh-my-posh/themes"
-        destination_dir="$HOME/.local/share/oh-my-posh/themes"
-
-        if [ -d "$source_dir" ]; then
-            mv "$source_dir" "$destination_dir"
-            echo "Themes moved from $source_dir to $destination_dir"
-        else
-            echo "Source directory $source_dir not found."
-            echo "Try installing oh-my-posh again manually "
-        fi
-
-        ohmyposh_path="$HOME/.oh-my-posh"
-        ohmyposh_theme_command="eval \"\$(oh-my-posh init bash --config $HOME/.local/share/oh-my-posh/themes/catppuccin_macchiato.omp.json)\""
-
-        # Adding the paths and configurations to .bashrc
-        if ! grep -q "export PATH=\"$ohmyposh_path\"" "$HOME/.bashrc"; then
-            echo "export PATH=\"$ohmyposh_path\"" >> "$HOME/.bashrc"
-        fi
-
-        if ! grep -q "$ohmyposh_theme_command" "$HOME/.bashrc"; then
-            echo "$ohmyposh_theme_command" >> "$HOME/.bashrc"
-        fi
-
-        # Source the updated .bashrc
-        source "$HOME/.bashrc"
-
-        cd ..
-
-        echo "Moving config files......"
- 
+        # If contributed add it here please
         ;;
     *)
-        echo "Distribution not recognized. This install script is only made for Arch , Fedora and Ubuntu."
+        echo "$ERR Distribution not supported. This install script is only made for Arch"
         ::
 esac
 
 #step 2 - configure the window manager
-
-# Custom Functions
-if_dir_exists() {
-    local dir_path="$1"
-
-    if [ -d "$dir_path" ]; then
-        :  # Directory exists
-    else
-        mkdir "$dir_path"  # Directory does not exist
-    fi
-} 
---
+++ scripts/arch.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+# Moving Shell command to different files to use them as modules
+
+# Basic Design 
+# - Create a log file where all the install outputs store and only a simplified output is seen
+# - Be bit more defensive to errors
+# - Complete by this month as i have lot to do 
+
+# Few Predefined stuff
+NOT="[\e[1;36mNOTE\e[0m]"
+OK="[\e[1;32mOK\e[0m]"
+ERR="[\e[1;31mERROR\e[0m]"
+ATT="[\e[1;37mATTENTION\e[0m]"
+WARN="[\e[1;35mWARNING\e[0m]"
+ACT="[\e[1;33mACTION\e[0m]"
+LOGIT="install.log"
+
+# Custom Functions
+if_dir_exists() {
+    local dir_path="$1"
+
+    if [ -d "$dir_path" ]; then
+        :  # Directory exists
+    else
+        mkdir "$dir_path"  # Directory does not exist
+    fi
+} 
+
+echo "Installing i3, rofi, feh, neofetch.........."
+        sudo pacman -s neofetch i3-wm rofi rofi-emoji feh alacritty picom 
+
+        read -p "Do you want to install oh-my-posh? (y/n): " opinion
+        if [ "$opinion" = "y" ] || [ "$opinion" = "Y" ]; then
+            echo "Installing Oh-my-posh......."
+            #Installing Oh-my-posh
+        
+            curl -s https://ohmyposh.dev/install.sh | bash -s -- -d ./
+
+            if_dir_exists "$HOME/.oh-my-posh"
+            if_dir_exists "$HOME/.local/share/oh-my-posh/themes"
+            
+            cp oh-my-posh $HOME/.oh-my-posh
+
+            source_dir="$HOME/.cache/oh-my-posh/themes"
+            destination_dir="$HOME/.local/share/oh-my-posh/themes"
+
+            if [ -d "$source_dir" ]; then
+                mv "$source_dir" "$destination_dir"
+                echo "Themes moved from $source_dir to $destination_dir"
+            else
+                echo "Source directory $source_dir not found."
+                echo "Try installing oh-my-posh again manually "
+            fi
+
+            ohmyposh_path="$HOME/.oh-my-posh"
+            ohmyposh_theme_command="eval \"\$(oh-my-posh init bash --config $HOME/.local/share/oh-my-posh/themes/catppuccin_macchiato.omp.json)\""
+
+            # Adding the paths and configurations to .bashrc
+            if ! grep -q "export PATH=\"$ohmyposh_path\"" "$HOME/.bashrc"; then
+                echo "export PATH=\"$ohmyposh_path\"" >> "$HOME/.bashrc"
+            fi
+
+            if ! grep -q "$ohmyposh_theme_command" "$HOME/.bashrc"; then
+                echo "$ohmyposh_theme_command" >> "$HOME/.bashrc"
+            fi
+
+            # Source the updated .bashrc
+            source "$HOME/.bashrc"
+        else
+            echo "Oh-my=posh was not installed ..."
+        fi
+        
+        echo "Moving config files......"
